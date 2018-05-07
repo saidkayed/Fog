b6cee2be0a2549af37a95d889694559249a23ba9
saidkayed <36620432+saidkayed@users.noreply.github.com>
2018-05-07 09:58:07 +0200

    class added

---
+++ src/main/java/DBAccess/MaterialeMapper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBAccess;
+
+/**
+ *
+ * @author sidad
+ */
+public class MaterialeMapper {
+    
+    public static void createMateriale(){
+        
+    }
+    
+    public static void deleteMateriale(){
+        
+    }
+    
+}
